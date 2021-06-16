8b87df5f0a56a26a0b491076f8c8e2fb0917bb50
Adetoba Adedeji <adetoba22@gmail.com>
2021-06-16 12:35:54 +0100

    menu-items updated with withRouter to navigate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+
 function App() {
   return (
-    <div><HomePage/></div>
+    <div>
+      <switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </switch>
+    </div>
   )
 }
 
